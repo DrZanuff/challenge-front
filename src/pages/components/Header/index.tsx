45a005cdabd064c4447f8ddb755922172663c104
Ricardo <ricardo.machado.nwi@gmail.com>
2021-07-28 10:34:34 -0300

    feat(Main menu on mobile ): There is now an animation for this menu

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { useState , useEffect } from 'react';
-import { Search , Person , ShoppingCart , Menu } from '@material-ui/icons';
+import { Search , Person , ShoppingCart , Menu , Close} from '@material-ui/icons';
 import Link from 'next/link';
 import styles from './styles.module.scss';
 
@@ -7,6 +7,7 @@ export default function Header() {
     const minDesktopSize = 1280;
 
     const [isDesktop, setDesktop] = useState(true);
+    const [isMobileMenuOpen , setIsMobileMenuOpen] = useState(false);
 
     useEffect(() => {
         window.addEventListener("resize", ()=>{ 
@@ -16,37 +17,56 @@ export default function Header() {
 
 
     return (
-        
-        <header className={styles.container}>
-            <div className={styles.content}>
-                {
-                !isDesktop &&
-                <Menu fontSize="large"/>
-                }
+        <>
+            <header className={styles.container}>
+                <div className={styles.content}>
+                    {
+                    !isDesktop &&
+                    <div onClick={()=> setIsMobileMenuOpen(true)}>
+                        <Menu fontSize="large" />
+                    </div>
+                    }
 
-                <img src="/images/logo/agencia-eplus-n-logo.png" alt="Logo Eplus"></img>
+                    <img src="/images/logo/agencia-eplus-n-logo.png" alt="Logo Eplus"></img>
 
-                {
-                isDesktop &&
+                    {
+                    isDesktop &&
+                    <nav>
+                        <Link href="/"><a>Lorem Ispum</a></Link>
+                        <Link href="/"><a>Lorem Ispum</a></Link>
+                        <Link href="/"><a>Lorem Ispum</a></Link>
+                        <Link href="/"><a>Lorem Ispum</a></Link>
+                    </nav>
+                    }
+
+                    <div className={styles.user}>
+                        <Search fontSize="small"></Search>
+                        <Person fontSize="small"></Person>
+                        <ShoppingCart fontSize="small"></ShoppingCart>
+                    </div>
+
+                </div>
+
+            </header>
+            { 
+            isMobileMenuOpen &&
+            <div 
+            className={styles.openMenu}
+            onClick={()=> setIsMobileMenuOpen(false)}
+            onMouseLeave={()=> setIsMobileMenuOpen(false)}
+            >
+                <div onClick={()=> setIsMobileMenuOpen(false)}>
+                    <Close fontSize="large" />
+                </div>
                 <nav>
                     <Link href="/"><a>Lorem Ispum</a></Link>
                     <Link href="/"><a>Lorem Ispum</a></Link>
                     <Link href="/"><a>Lorem Ispum</a></Link>
                     <Link href="/"><a>Lorem Ispum</a></Link>
                 </nav>
-                }
-
-                <div className={styles.user}>
-                    <Search fontSize="small"></Search>
-                    <Person fontSize="small"></Person>
-                    <ShoppingCart fontSize="small"></ShoppingCart>
-                </div>
-
             </div>
+            }
 
-
-
-
-        </header>
+        </>
     )
 }
